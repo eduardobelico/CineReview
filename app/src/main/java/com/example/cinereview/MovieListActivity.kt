0df285485827e6e9f08c101d33c40dcadfe8fe1f
Eduardo Bélico <116230274+eduardobelico@users.noreply.github.com>
2023-04-19 15:55:38 -0300

    Commit Inicial

---
+++ app/src/main/java/com/example/cinereview/MovieListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cinereview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MovieListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
